4849d54a34dda05f3a679e91c9d19b9c5eb7a94a
bez <Behzad.p.ashrafi@gmail.com>
2024-10-30 22:39:21 +0300

    component init

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultComponent } from './layouts/default/default.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DefaultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
